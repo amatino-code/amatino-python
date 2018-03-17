c640445e9c54ff74c041b308e6f3393d8bb73428
Hugh Jeremy <hugh.jeremy@gmail.com>
2018-03-17 22:45:49 +1100

    data package initialisation

--- amatino/_internal/_data_package.py
+++ amatino/_internal/_data_package.py
@@ -15,8 +15,29 @@ class _DataPackage:
     Abstract class providing JSON data packing capabilities
     to concrete classes.
     """
-    def __init__(self):
-        self._package = None
+    def __init__(self,
+        list_data: list = None,
+        object_data: dict = None,
+        override_listing: bool = False
+        ):
+    
+        self._data = None
+
+        assert isinstance(override_listing, bool)
+        
+        if list_data is not None:
+            assert isinstance(list_data, list)
+            assert override_listing is False
+            self._data = list_data
+        
+        if object_data is not None:
+            assert isinstance(object_data, dict)
+            if override_listing is True:
+                self._data = object_data
+            else:
+                self._data = [object_data]
+
+        assert self._data is not None
         return
 
     def as_json_bytes(self) -> bytes:
@@ -24,5 +45,5 @@ class _DataPackage:
         Return package arguments as JSON bytes suitable
         for inclusion in an HTTP request.
         """
-        data = json.dumps(self._package).encode('utf-8')
+        data = json.dumps(self._data).encode('utf-8')
         return data
