64566b2e5fba629d7e6ebb415d1e09b04d39ded3
Hugh Jeremy <hugh.jeremy@gmail.com>
2018-08-22 18:21:37 +1000

    remove old files

--- amatino/entity.py
+++ amatino/entity.py
@@ -5,7 +5,17 @@ Author: hugh@amatino.io
 """
 from amatino import Session
 from amatino.region import Region
-from amatino._internal._new_entity_arguments import _NewEntityArguments
+from amatino.internal.new_entity_arguments import NewEntityArguments
+from amatino.internal.api_request import ApiRequest
+from amatino.internal.data_package import DataPackage
+from amatino.internal.http_method import HTTPMethod
+from amatino.amatino_error import AmatinoError
+from typing import TypeVar
+from typing import Optional
+from typing import Any
+
+T = TypeVar('T', bound='Entity')
+
 
 class Entity:
     """
@@ -14,45 +24,103 @@ class Entity:
 
     An example of an entity might be a legal company, a consolidated group
     of companies, a project, or even a person.
-
-    You may initialise an Entity object in one of two ways:
-
-    1.  Retrieve an existing entity, by supplying a string entity identifier
-        to the entity_id parameter.
-
-    2.  Create a new entity, by supplying all parameters.
-
     """
+    PATH = '/entities'
+    MAX_NAME_LENGTH = NewEntityArguments.MAX_NAME_LENGTH
+    MAX_DESCRIPTION_LENGTH = NewEntityArguments.MAX_DESCRIPTION_LENGTH
+
     def __init__(
-            self,
-            session: Session,
-            entity_id: str,
-            name: str = None,
-            description: str = None,
-            region: Region = None
-        ):
+        self,
+        session: Session,
+        entity_id: str,
+        name: str,
+        description: str,
+        region_id: int,
+        owner_id: int,
+        active: bool,
+        permissions_graph: {str: {str: {str: bool}}}
+    ) -> None:
 
         self._session = session
-        self._new_entity_arguments = None
+        self._entity_id = entity_id
+        self._name = name
+        self._description = description
+        self._region_id = region_id
+        self._owner_id = owner_id
+        self._active = active
+        self._permissions_graph = permissions_graph
 
-        if (
-                name is not None
-                or description is not None
-                or region is not None
-        ):
-            self._new_entity_arguments = _NewEntityArguments(
-                entity_id,
-                name,
-                description,
-                region
+        raise NotImplementedError
+
+    @classmethod
+    def create(
+        cls: type,
+        session: Session,
+        name: str,
+        description: str,
+        region: Optional[Region] = None
+    ) -> T:
+
+        new_entity_arguments = NewEntityArguments(
+            name,
+            description,
+            region
+        )
+
+        request_data = DataPackage.from_object(new_entity_arguments)
+
+        request = ApiRequest(
+            Entity.PATH,
+            HTTPMethod.POST,
+            session._credentials(),
+            request_data,
+            None,
+            False
+        )
+
+        created_entity = Entity._decode(request.response_data, session)
+
+        return created_entity
+
+    @classmethod
+    def _decode(cls: type, data: list, session: Session) -> T:
+        """
+        Return an Entity instance decoded from API response data
+        """
+        assert isinstance(session, Session)
+
+        if not isinstance(data, list):
+            raise AmatinoError('Unexpected response format: ' + str(type(data)))
+
+        if len(data) < 1:
+            raise AmatinoError('Response list unexpectedly empty')
+
+        raw_entity = data[0]
+
+        if not isinstance(raw_entity, dict):
+            raise AmatinoError('Unexpected response format')
+        try:
+            entity = Entity(
+                session=session,
+                entity_id=raw_entity['session_id'],
+                name=raw_entity['name'],
+                description=raw_entity['description'],
+                region_id=raw_entity['storage_region'],
+                owner_id=raw_entity['owner'],
+                active=raw_entity['active'],
+                permissions_graph=raw_entity['permissions_graph']
             )
 
-            self._create()
+        except KeyError:
+            raise AmatinoError('Unexpected response format, missing a key')
+        return entity
 
-            return
-
-        self._entity_id = entity_id
-        self._retrieve()
+    @classmethod
+    def retrieve(
+        cls,
+        session: Session,
+        entity_id: str
+    ) -> T:
 
         return
 
