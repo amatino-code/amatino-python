4ad8510d4eea7a59b7274c56e0a38123b33dca55
Hugh Jeremy <hugh.jeremy@gmail.com>
2018-08-28 13:19:43 +1000

    implement hex transforms in Color

--- amatino/color.py
+++ amatino/color.py
@@ -3,6 +3,12 @@ Amatino API Python Bindings
 Color Module
 Author: hugh@amatino.io
 """
+from typing import TypeVar
+from typing import Type
+from amatino.internal.immutable import Immutable
+
+T = TypeVar('T', bound='Color')
+
 
 class Color:
     """
@@ -13,7 +19,7 @@ class Color:
         red: int,
         green: int,
         blue: int
-    ):
+    ) -> None:
 
         if (
                 not isinstance(red, int)
@@ -28,13 +34,34 @@ class Color:
                 blue < 0 or blue > 255
         ):
             raise ValueError('Color components must be >= 0 and <= 255')
-        
+
         self._red = red
         self._green = green
         self._blue = blue
 
         return
-    
+
+    red: int = Immutable(lambda s: s._red)
+    green: int = Immutable(lambda s: s._green)
+    blue: int = Immutable(lambda s: s._blue)
+
+    @classmethod
+    def from_hex_string(cls: Type[T], hexstring: str) -> T:
+
+        if not isinstance(hexstring, str):
+            raise TypeError('hexstring must be of type `str`')
+
+        if len(hexstring) != 6:
+            raise ValueError('hexstring must be 6 characters')
+
+        red = int(hexstring[:2], 16)
+        green = int(hexstring[2:4], 16)
+        blue = int(hexstring[4:6], 16)
+
+        color = cls(red=red, green=green, blue=blue)
+
+        return color
+
     def as_hex_string(self) -> str:
         """
         Return the colour as a hex value string
