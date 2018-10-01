d6f780cd47be55ff28d6f089b06cc67d794a3375
Hugh Jeremy <hugh.jeremy@gmail.com>
2018-10-01 16:36:10 +1000

    add missing properties

--- amatino/denomination.py
+++ amatino/denomination.py
@@ -11,15 +11,27 @@ class Denomination:
     Abstract class defining an interface for units of account. Adopted by
     Custom Units and Global Units.]
     """
-    def __init__(self, code: str, id_: int, name: str) -> None:
-        assert isinstance(code, str)
-        assert isinstance(id_, int)
-        assert isinstance(name, str)
+    def __init__(
+        self,
+        code: str,
+        id_: int,
+        name: str,
+        priority: int,
+        description: str,
+        exponent: int
+    ) -> None:
+
         self._code = code
         self._id = id_
         self._name = name
+        self._priority = priority
+        self._description = description
+        self._exponent = exponent
         return
 
     code = Immutable(lambda s: s._code)
     id_ = Immutable(lambda s: s._id)
     name = Immutable(lambda s: s._name)
+    priority = Immutable(lambda s: s._priority)
+    description = Immutable(lambda s: s._description)
+    exponent = Immutable(lambda s: s._exponent)
