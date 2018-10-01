9d67a188d7aab183136d0f8ae3c7d36d381f9d05
Hugh Jeremy <hugh.jeremy@gmail.com>
2018-10-01 16:35:12 +1000

    new helper class methods

--- amatino/internal/url_target.py
+++ amatino/internal/url_target.py
@@ -3,24 +3,39 @@ Amatino API Python Bindings
 Url Target Module
 Author: hugh@amatino.io
 """
+from typing import Type
+from typing import TypeVar
+
+T = TypeVar('T', bound='UrlTarget')
 
 
 class UrlTarget:
-    """
-    Internal class handling individual url parameter
-    components
-    """
-    def __init__(self, key: str, value: str):
+    """Internal class handling individual url parameter components"""
 
-        assert isinstance(key, str)
-        if (
-                not isinstance(value, str)
-                and not isinstance(value, int)
-        ):
-            raise AssertionError('Value must be of type int or str')
+    def __init__(self, key: str, value: str) -> None:
+
+        if not isinstance(key, str):
+            raise TypeError('Key must be of type `str`')
+
+        if not isinstance(value, str):
+            raise TypeError('value must be of type `str`')
 
         self.key = key
         self.value = value
+
+        return
+
+    @classmethod
+    def from_integer(
+        cls: Type[T],
+        key: str,
+        value: int,
+    ) -> T:
+
+        if not isinstance(value, int):
+            raise TypeError('value must be of type `int`')
+
+        return cls(key, str(value))
 
     def __str__(self):
         return self.key + '=' + str(self.value)
