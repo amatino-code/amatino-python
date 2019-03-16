f4279e61c5658b98de7a7f3b1ac2febc7a9edc01
Hugh Jeremy <hugh.jeremy@gmail.com>
2019-03-16 11:27:07 +0800

    add test environment help message

--- amatino/tests/test.py
+++ amatino/tests/test.py
@@ -13,6 +13,26 @@ from os import environ
 import traceback
 
 
+ENVIRONMENT_HELP_MESSAGE = """
+Amatino's test suite requires a few environment variables to run. It looks like
+you are missing one or more of them. The required variables are:
+
+AMATINO_TEST_USER_ID - Integer user id
+AMATINO_TEST_EMAIL - String user email
+AMATINO_TEST_SECRET - String user passphrase (take care not to save on disk!)
+
+These three variable must be associated with a valid Amatino user account, with
+active billing.
+
+You can add the variables to your environment using the `export` command. For
+example:
+
+$ export AMATINO_TEST_EMAIL=some.cool@email.org
+
+Once you've added the required variables, try running the test suite again.
+"""
+
+
 class Test:
     """
     Abstract class offering base-level functionality to tests.
@@ -22,9 +42,13 @@ class Test:
         if not isinstance(test_name, str):
             raise TypeError('test_name must be of type `str`')
 
-        raw_user_id = environ['AMATINO_TEST_USER_ID']
-        email = environ['AMATINO_TEST_EMAIL']
-        secret = environ['AMATINO_TEST_SECRET']
+        try:
+            raw_user_id = environ['AMATINO_TEST_USER_ID']
+            email = environ['AMATINO_TEST_EMAIL']
+            secret = environ['AMATINO_TEST_SECRET']
+        except KeyError as error:
+            print(ENVIRONMENT_HELP_MESSAGE)
+            quit()
 
         if raw_user_id is None:
             raise RuntimeError('AMATINO_TEST_USER_ID env variable required')
