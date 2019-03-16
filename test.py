a0649978567bc23f16d23987e4a554cf55e4231b
Hugh Jeremy <hugh.jeremy@gmail.com>
2019-03-16 17:16:33 +0800

    add test initialisation message

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
+EXECUTION_MESSAGE = """Beginning Amatino Python test sequence"""
+
 if __name__ == '__main__':
     from amatino.tests.test_sequence import SEQUENCE
+    print(EXECUTION_MESSAGE)
     for test in SEQUENCE:
         instance = test()
         instance.execute()
