a4d3ebace8fcdb04c6a95cd1bd8f622dffcc41d1
Hugh Jeremy <hugh.jeremy@gmail.com>
2019-03-17 16:07:30 +0800

    add Performance test

--- amatino/tests/derived/__init__.py
+++ amatino/tests/derived/__init__.py
@@ -2,3 +2,4 @@ from amatino.tests.derived.ledger import LedgerTest
 from amatino.tests.derived.recursive_ledger import RecursiveLedgerTest
 from amatino.tests.derived.balance import BalanceTest
 from amatino.tests.derived.recursive_balance import RecursiveBalanceTest
+from amatino.tests.derived.performance import PerformanceTest
---
+++ amatino/tests/derived/performance.py
@@ -0,0 +1,140 @@
+"""
+Amatino API Python Bindings
+Performance class Test Module
+Author: hugh@blinkybeach.com
+"""
+from amatino.tests.primary.account import AccountTest
+from amatino import Account
+from amatino import Transaction
+from amatino import Performance
+from amatino import AMType
+from amatino import Side
+from amatino import Entry
+from amatino import GlobalUnit
+from amatino import TreeNode
+from amatino import Entity
+from datetime import datetime
+from datetime import timedelta
+from decimal import Decimal
+
+NAME = 'Retrieve a Performance'
+
+
+class PerformanceTest(AccountTest):
+    """Test the Performance object"""
+
+    def __init__(self, name=NAME) -> None:
+        super().__init__(name)
+        return
+
+    def execute(self) -> None:
+
+        try:
+
+            income = self.create_account(
+                amt=AMType.income,
+                name='Test Income'
+            )
+
+            income_child = Account.create(
+                self.session,
+                self.entity,
+                'Test income child',
+                income.am_type,
+                income.denomination
+            )
+
+            expense = self.create_account(
+                amt=AMType.expense,
+                name='Test Expense'
+            )
+
+            Transaction.create(
+                self.session,
+                self.entity,
+                datetime.utcnow(),
+                [
+                    Entry(Side.debit, Decimal('400'), income),
+                    Entry(Side.credit, Decimal('400'), expense)
+                ],
+                self.usd,
+                'Test transaction 1'
+            )
+
+            Transaction.create(
+                self.session,
+                self.entity,
+                datetime.utcnow(),
+                [
+                    Entry(Side.debit, Decimal('250'), income_child),
+                    Entry(Side.debit, Decimal('250'), income),
+                    Entry(Side.credit, Decimal('500'), expense)
+                ],
+                self.usd,
+                'Test transaction 2'
+            )
+
+            Transaction.create(
+                self.session,
+                self.entity,
+                datetime.utcnow() - timedelta(days=4),
+                [
+                    Entry(Side.debit, Decimal('1'), income),
+                    Entry(Side.credit, Decimal('1'), expense)
+                ],
+                self.user,
+                'Test transaction 3'
+            )
+
+            Transaction.create(
+                self.session,
+                self.entity,
+                datetime.utcnow() + timedelta(days=4),
+                [
+                    Entry(Side.debit, Decimal('50'), income),
+                    Entry(Side.credit, Decimal('50'), expense)
+                ],
+                self.user,
+                'Test transaction 4'
+            )
+
+            performance = Performance.retrieve(
+                self.entity,
+                Performance.RetrieveArguments(
+                    datetime.utcnow() - timedelta(days=2),
+                    datetime.utcnow() + timedelta(days=2),
+                    self.usd
+                )
+            )
+
+            assert isinstance(performance, Performance)
+            assert performance.has_income
+            assert performance.has_expense
+            assert performance.global_unit_id == self.usd.id_
+            assert performance.custom_unit_id is None
+            assert isinstance(performance.denomination, GlobalUnit)
+            assert isinstance(performance.start_time, datetime)
+            assert isinstance(performance.end_time, datetime)
+            assert isinstance(performance.generated_time, datetime)
+            assert isinstance(performance.entity, Entity)
+
+            income_root = performance.income[0]
+            expense_root = performance.expenses[0]
+
+            assert isinstance(income_root, TreeNode)
+            assert isinstance(expense_root, TreeNode)
+            assert income_root.has_children
+            assert not expense_root.has_children
+            assert isinstance(income_root.children[0], TreeNode)
+
+            assert income_root.recursive_balance == Decimal('900')
+            assert income_root.account_balance == Decimal('650')
+            assert income_root.children[0].account_balance == Decimal('250')
+            assert expense_root.account_balance == Decimal('900')
+            assert expense_root.recursive_balance == Decimal('900')
+
+        except Exception as error:
+            self.record_failure(error)
+            return
+
+        self.record_success()
