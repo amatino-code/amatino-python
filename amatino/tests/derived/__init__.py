0e9be6938781d02759b8c8745af3eb17b9d55a4e
Hugh Jeremy <hugh.jeremy@gmail.com>
2019-03-17 16:58:19 +0800

    add PositionTest to initialisation

--- amatino/tests/derived/__init__.py
+++ amatino/tests/derived/__init__.py
@@ -3,3 +3,4 @@ from amatino.tests.derived.recursive_ledger import RecursiveLedgerTest
 from amatino.tests.derived.balance import BalanceTest
 from amatino.tests.derived.recursive_balance import RecursiveBalanceTest
 from amatino.tests.derived.performance import PerformanceTest
+from amatino.tests.derived.position import PositionTest
